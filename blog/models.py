0e38e3da1720d6544ecc2bce5ffcaf4778193b09
Irina Kasatkina <kasatkina235@gmail.com>
2022-10-04 14:53:56 +0300

    reduce db queries number

--- blog/models.py
+++ blog/models.py
@@ -8,21 +8,21 @@ class PostQuerySet(models.QuerySet):
     def popular(self):
         return self.annotate(likes_count=Count('likes')).order_by('-likes_count')
 
-    def fetch_with_comments_count(most_popular_posts):
-        """Функция для присоединения к каждому из популярных постов псевдо-поля
+    def fetch_with_comments_count(posts):
+        """Функция для присоединения к каждому из постов запроса псевдо-поля
            comments_count с количеством комментариев к посту.
            Вызывать эту функцию предпочтительнее, чем использовать второй annotate
            в запросе, потому что два annotate приводят к очень долгому выполнению запроса.
         """
 
-        most_popular_posts_ids = [post.id for post in most_popular_posts]
-        posts_with_comments = (Post.objects.filter(id__in=most_popular_posts_ids)
-                                           .annotate(comments_count=Count('comments')))
-        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        posts_ids = [post.id for post in posts]
+        posts_with_comments_count = (Post.objects.filter(id__in=posts_ids)
+                                                 .annotate(comments_count=Count('comments')))
+        ids_and_comments = posts_with_comments_count.values_list('id', 'comments_count')
         count_for_id = dict(ids_and_comments)
-        for post in most_popular_posts:
+        for post in posts:
             post.comments_count = count_for_id[post.id]
-        return list(most_popular_posts)
+        return list(posts)
 
 
 class Post(models.Model):
